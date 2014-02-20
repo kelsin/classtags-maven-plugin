5b6df248c4e3aa3bbdf62722c60a36472c940117
Christopher Giroir <kelsin@valefor.com>
2014-02-20 00:10:52 -0800

    Got list of all jars/dirs... lots to do now

--- src/main/java/net/kelsin/ClassTags.java
+++ src/main/java/net/kelsin/ClassTags.java
@@ -16,17 +16,63 @@ package net.kelsin;
  * limitations under the License.
  */
 
+import java.lang.String;
+import java.util.List;
+
+import org.apache.maven.artifact.DependencyResolutionRequiredException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
 
 /**
  * Mojo which generates a .classtags file full of all classes in classpath
  */
-@Mojo(name = "generate")
+@Mojo(name = "generate", requiresDependencyResolution = ResolutionScope.TEST)
 public class ClassTags extends AbstractMojo {
+	@Parameter(property = "project", readonly = true, required = true)
+	private MavenProject project;
 
 	public void execute() throws MojoExecutionException {
 		getLog().info("Generating Classtags");
+
+		List<String> elements;
+
+		try {
+			elements = project.getRuntimeClasspathElements();
+
+			getLog().info("Found " + elements.size() + " Runtime Elements");
+			for(String element: elements) {
+				getLog().info(element);
+			}
+
+			elements = project.getCompileClasspathElements();
+
+			getLog().info("Found " + elements.size() + " Compile Elements");
+			for(String element: elements) {
+				getLog().info(element);
+			}
+
+			elements = project.getTestClasspathElements();
+
+			getLog().info("Found " + elements.size() + " Test Elements");
+			for(String element: elements) {
+				getLog().info(element);
+			}
+
+			elements = project.getSystemClasspathElements();
+
+			getLog().info("Found " + elements.size() + " System Elements");
+			for(String element: elements) {
+				getLog().info(element);
+			}
+		} catch (DependencyResolutionRequiredException e) {
+			getLog().error("Couldn't get dependencies");
+		}
+
+		getLog().info(System.getProperty("java.ext.dirs"));
+		getLog().info(System.getProperty("sun.boot.class.path"));
 	}
 }
